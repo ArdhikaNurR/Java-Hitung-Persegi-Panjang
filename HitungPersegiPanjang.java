d3eef5afe7406d63920dc36a1cf017c86513fff6
ArdhikaNurR <ardhika.nr@gmail.com>
2021-08-30 20:14:47 +0700

    Menambahkan file Main java hitung persegi panjang

---
+++ HitungPersegiPanjang.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.studikasus;
+
+/**
+ *
+ * @author Ardhika NR
+ */
+public class HitungPersegiPanjang {
+    public static void main(String[] args) {
+        PersegiPanjang pp1 = new PersegiPanjang(8, 9);
+        pp1.Show();
+        pp1.getLuas();
+        pp1.getKeliling();
+    }
+}
